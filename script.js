e8ed04e2c1876d4584022068ba064b322effce46
Suhrob701 <suhrobnurmamatov909@gmail.com>
2024-11-08 20:38:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    
+let news1 = prompt('String typedagi malumotni kiriting');
+    let news2 = prompt('Number typedagi malumotni kiriting');
+    let news3 = prompt('Boolean typedagi malumotni kiriting');
+    confirm("Hamma ma'lumotlarni to'g'ri kiritganingizga ishonchingiz komilmi?") 
+    
+    alert(`Siz kiritgan ${news1} sozi String typega tegishli.\n`
+        +  `Siz kiritga  ${news2} sozi Number typega tegishli.\n`
+        + `Siz kiritgan ${news3} sozi Boolean typega tegishli.\n`);
